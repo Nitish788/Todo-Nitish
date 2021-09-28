82dddcd9a5452df3c46c1bbc199e31c164b14c44
Nitish788 <nsinghrajput78@gmail.com>
2021-09-28 12:38:37 +0530

    add local storage

--- script.js
+++ script.js
@@ -2,35 +2,124 @@ const input = document.querySelector(".what_todo");
 
 const btnAdd = document.querySelector(".add");
 
-const btnDelete = document.getElementsByClassName("delete");
-
 const todoList = document.querySelector(".todo__list");
 
-const todo = [];
+const labelRight = document.querySelector(".right");
 
-const list = function(arr) {
-    btnAdd.addEventListener("click", function() {
-        let work = input.value;
+const labelLeft = document.querySelector(".left");
 
-        if (!work) return;
-        todo.push(work);
-        todoList.innerHTML = " ";
+const cancel = document.querySelector(".clear");
 
-        arr.forEach((el) => {
-            const html = `<div class="todo">
-        ${el} <button class="delete">A</button>
-        </div>`;
-            todoList.insertAdjacentHTML("afterbegin", html);
-        });
-        input.value = "";
-    });
+// Create an Array for both localStorage and for script
+
+const store = function() {
+    let work = input.value;
+    if (!work) return;
+    let localItems = JSON.parse(localStorage.getItem("localItem"));
+    if (localItems === null) {
+        task = [];
+    } else {
+        task = localItems;
+    }
+    task.push(work);
+    localStorage.setItem("localItem", JSON.stringify(task));
+    addList();
+    input.value = "";
 };
 
-list(todo);
+//  Add event listner to add work
 
-todoList.addEventListener("click", function(e) {
+btnAdd.addEventListener("click", store);
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") store();
+});
+
+//  Create Todo List by inserting in DOM
+
+function addList() {
+    let localItems = JSON.parse(localStorage.getItem("localItem"));
+    if (localItems === null) {
+        task = [];
+    } else {
+        task = localItems;
+    }
+    labelLeft.innerHTML = " ";
+    labelRight.innerHTML = " ";
+    console.log(task);
+    task.forEach((el, i) => {
+        if (i % 2 === 0) {
+            let html = `<div class="todo"><span class ="hide">${el}</span><button class="delete"><img src="delete.png" alt="" class ="delete"/></button>
+    </div>`;
+            labelLeft.insertAdjacentHTML("beforeend", html);
+        } else {
+            html = `<div class="todo"><span class ="hide">${el}</span><button class="delete"><img src="delete.png" alt="" class ="delete"/></button>
+        </div>`;
+            labelRight.insertAdjacentHTML("beforeend", html);
+        }
+    });
+}
+addList();
+
+// Add event Listner to Parent element (event delegation) and select by specific condition (DOM traversing) for delete specific work
+
+const deleteTask = function(e) {
     if (e.target.classList.contains("delete")) {
         const curr = e.target.closest(".todo");
+
         curr.remove();
+
+        task.forEach((el, i) => {
+            const current = curr.firstElementChild.innerText;
+
+            console.log(el === current);
+            if (el === current) {
+                let localItems = JSON.parse(localStorage.getItem("localItem"));
+
+                task.splice(i, 1);
+                localStorage.setItem("localItem", JSON.stringify(task));
+                addList();
+            }
+        });
     }
+};
+labelLeft.addEventListener("click", deleteTask);
+labelRight.addEventListener("click", deleteTask);
+
+// Add Event Listner for clear whole list
+
+cancel.addEventListener("click", function() {
+    console.log("cl");
+    localStorage.clear();
+    addList();
 });
+
+//  Get current date and day
+
+const labelDay = document.querySelector(".day");
+const labelDate = document.querySelector(".date");
+const option = {
+    weekday: "long",
+};
+const option2 = {
+    day: "numeric",
+    year: "numeric",
+    month: "short",
+};
+const day = new Date();
+
+setInterval(() => {
+    labelDay.textContent = new Intl.DateTimeFormat(
+        navigator.language,
+        option
+    ).format(day);
+    labelDate.textContent = new Intl.DateTimeFormat("en-GB", option2).format(day);
+}, 1000);
+
+if (labelRight.innerText === "" || labelLeft.innerText === "") {
+    console.log("hello");
+    cancel.classList.add("hidden");
+}
+if (labelRight.innerText !== "" || labelLeft.innerText !== "") {
+    cancel.classList.remove("hidden");
+}
+console.log(labelLeft.innerText === "");
